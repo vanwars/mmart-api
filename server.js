e7747e52e25cd4aa1e53d92426d3f6d963e623dd
vagrant <vagrant@precise32.(none)>
2016-11-16 04:21:27 +0000

    added file uploader

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
-
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+};
 var app = express();
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
@@ -58,8 +64,51 @@ app.get("/contacts", function(req, res) {
   });
 });
 
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+app.post('/file-upload', multipartMiddleware, function(req, res) {
+    console.log(req.body, req.files);
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = '/tmp/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.status(201).json({
+                "message": 'File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes'
+            });
+        });
+    });
+});
+
+/*app.post('/file-upload', function(req, res) {
+    
+    console.log("REQ.BODY", req.body);
+    console.log("REQ.FILES", req.files);
+    console.log("REQ", req);
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = './public/images/' + req.files.thumbnail.name;
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+});*/
+
 app.post("/contacts", function(req, res) {
   var newContact = req.body;
+  console.log(req);
+  console.log(req.body);
   newContact.createDate = new Date();
 
   if (!(req.body.firstName || req.body.lastName)) {
@@ -70,6 +119,7 @@ app.post("/contacts", function(req, res) {
     if (err) {
       handleError(res, err.message, "Failed to create new contact.");
     } else {
+        console.log(doc.ops[0]);
       res.status(201).json(doc.ops[0]);
     }
   });
